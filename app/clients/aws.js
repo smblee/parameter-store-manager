e45fa048abcd3febd4db6d5437c3f93343cf9327
Majid Burney <majid.burney@leafly.com>
2019-09-05 08:46:02 -0700

    Use credential provider chain to allow credentials_process (#102)
    * Use credential provider chain to allow credentials_process
    
    * Combining Credential Providers into AWS Clients
    
    Baked credential providers into the AWS client generator fns and added comments to guide future credential providers

--- app/clients/aws.js
+++ app/clients/aws.js
@@ -1,18 +1,21 @@
 import AWS from 'aws-sdk';
-import memoizeOne from 'memoize-one';
 import localStore from '../store/localStore';
 
 process.env.AWS_SDK_LOAD_CONFIG = true;
 
-const credentials = new AWS.SharedIniFileCredentials({
-  profile: localStore.get('profile')
-});
-if (typeof credentials !== 'undefined' && credentials) {
-  AWS.config.credentials = credentials;
-}
+const credentialProvider = new AWS.CredentialProviderChain([
+  () => new AWS.EnvironmentCredentials('AWS'),
+  () => new AWS.EnvironmentCredentials('AMAZON'),
+  () =>
+    new AWS.SharedIniFileCredentials({ profile: localStore.get('profile') }),
+  () => new AWS.ProcessCredentials({ profile: localStore.get('profile') })
+  // TODO: Add more credential providers as needed. https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CredentialProviderChain.html#providers-property
+]);
 
-const ssm = memoizeOne(region => new AWS.SSM({ region }));
-const kms = memoizeOne(region => new AWS.KMS({ region }));
+const ssm = region =>
+  new AWS.SSM({ region, credentials: null, credentialProvider });
+const kms = region =>
+  new AWS.KMS({ region, credentials: null, credentialProvider });
 
 const getSSM = () => ssm(localStore.get('ssmRegion'));
 const getKMS = () => kms(localStore.get('kmsRegion'));
